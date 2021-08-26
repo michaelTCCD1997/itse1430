788cb7776a7302fcc66d5272b6844f429f609dae
michaelTCCD1997 <89432238+michaelTCCD1997@users.noreply.github.com>
2021-08-25 19:05:49 -0700

    Update Program.cs

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            xyzConsole.WriteLine("Hello World!");
         }
     }
 }
