5946411d0529b46e2d8ca9a3c5fc1d39cef73b6e
michaelTCCD1997 <michael.quinlan@my.tccd.edu>
2021-08-30 19:35:18 -0700

    added movie lib post

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -4,13 +4,33 @@
  * Michael Quinlan
  * ITSE 1430 Fall 2021
  */
+using System;
+
 namespace HelloWorld
 {
     class Program
     {
         static void Main ( string[] args )
         {
+            int xyz;            
             Console.WriteLine("Hello World!");
+
+            //Declare some primitives
+            sbyte code = 10;
+            short port = 1234;
+            int hours = 45;
+            long debtPaybackInYears = 10_000;
+
+            double hoursWorkd = 80.9;
+            decimal payRate = 12.50M;
+
+            char letter = 'A';
+            string name = "Bob";
+
+            //Not primitive
+            DateTime today = DateTime.Now;
+            TimeSpan interval = TimeSpan.FromMinutes(10);
+            Guid identifier = Guid.Empty;
         }
     }
 }
---
+++ classwork/HelloWorld/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MovieLibrary.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Movie details
+            string title;
+            string description;
+           
+            int runLength;
+            int releaseYear;
+
+            double reviewRating;
+            string rating;
+            bool isClassic;
+        }
+    }
+}
